b3b8da0a0ad3d5d8c015d24e7126774042862e70
animesh-pal2003 <pal642057@gmail.com>
2025-05-04 04:06:09 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,17 @@
+//primitive (call by value )
+
+// 7 types : string ,Number , Boolean , Null , undefined , symbol ,BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn= false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+//reference or non primityive datatype 
+
+// types: arrays , objects , functions 
+
